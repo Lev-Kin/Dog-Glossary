2ab74fca007c5162cc827215f10ddada374db287
Lev <lev@lev>
2023-10-21 10:19:28 +0300

    3 of 5 stages completed

--- Dog Glossary/task/src/script.js
+++ Dog Glossary/task/src/script.js
@@ -3,18 +3,14 @@ document.getElementById('button-random-dog').addEventListener('click', () => {
         .then(response => response.json())
         .then(data => {
             const contentDiv = document.getElementById('content');
-
-            // Clear previous content
             contentDiv.innerHTML = '';
-
-            // Add new image
             const dogImage = document.createElement('img');
             dogImage.src = data.message;
             dogImage.alt = "Random Dog";
             contentDiv.appendChild(dogImage);
         })
         .catch(error => {
-            console.error("There was an error fetching the dog image:", error);
+            console.error("Error fetching the dog image:", error);
         });
 });
 
@@ -24,25 +20,45 @@ document.getElementById('button-show-breed').addEventListener('click', () => {
         .then(response => response.json())
         .then(data => {
             const contentDiv = document.getElementById('content');
-
-            // Clear previous content
             contentDiv.innerHTML = '';
-
-            // Check if the breed exists
             if (data.status === "error") {
                 contentDiv.innerHTML = '<p>Breed not found!</p>';
                 return;
             }
-
-            // Display the image of the specified breed
             const dogImage = document.createElement('img');
             dogImage.src = data.message;
             dogImage.alt = breed;
             contentDiv.appendChild(dogImage);
         })
         .catch(error => {
-            console.error("There was an error fetching the dog image:", error);
+            console.error("Error fetching the dog image:", error);
         });
 });
 
-const breed = document.getElementById('input-breed').value.trim().toLowerCase();
+document.getElementById('button-show-sub-breed').addEventListener('click', () => {
+    const breed = document.getElementById('input-breed').value.toLowerCase();
+    fetch(`https://dog.ceo/api/breed/${breed}/list`)
+        .then(response => response.json())
+        .then(data => {
+            const contentDiv = document.getElementById('content');
+            contentDiv.innerHTML = '';
+            if (data.status === "error") {
+                contentDiv.innerHTML = '<p>Breed not found!</p>';
+                return;
+            }
+            if (data.message.length === 0) {
+                contentDiv.innerHTML = '<p>No sub-breeds found!</p>';
+                return;
+            }
+            const ol = document.createElement('ol');
+            data.message.forEach(subBreed => {
+                const li = document.createElement('li');
+                li.textContent = subBreed;
+                ol.appendChild(li);
+            });
+            contentDiv.appendChild(ol);
+        })
+        .catch(error => {
+            console.error("Error fetching the sub-breeds:", error);
+        });
+});
